5b79c313d7d7e8a55c69283eb545737089cffc37
satabdi.chakraborty <satabdi.d@gmail.com>
2018-09-30 16:28:31 -0400

    First Commit

---
+++ src/main/java/org/satabdi/tutorial/springboot/CourseAPI/CourseApiApplication.java
@@ -0,0 +1,12 @@
+package org.satabdi.tutorial.springboot.CourseAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseApiApplication.class, args);
+	}
+}
